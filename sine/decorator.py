aeb06a32e48236191edc646c8145fae87f3c87a5
Sine <714186139@qq.com>
2017-07-13 21:13:18 +0800

    修改singleton（附带2个不用的版本在注释中）

--- sine/decorator.py
+++ sine/decorator.py
@@ -164,13 +164,74 @@ def acceptMultiIterable(stop_type=None, info_inject={}, max_depth=10):
         return decorated
     return decorator
 
-def singleton(clz):
+# 在A类中使用super(A...)...出现问题，因为A已经被更改，继承关系已经变了
+# def singleton(c):
+#     '''
+#     singleton pattern
+#     '''
+#     _instance = [None, False]
+#     class Singleton(c):
+#         def __new__(clz, *args, **kw):
+#             if _instance[0] == None:
+#                 _instance[0] = object.__new__(clz, *args, **kw)
+#             return _instance[0]
+#         def __init__(self, *args, **kw):
+#             if not _instance[1]:
+#                 _instance[1] = True
+#                 c.__init__(self, *args, **kw)
+#     return Singleton
+
+def singleton(C):
     '''
     singleton pattern
     '''
-    __instance = [None]
-    def singleton_deliver(*args, **kw):
-        if __instance[0] == None:
-            __instance[0] = clz(*args, **kw)
-        return __instance[0]
-    return singleton_deliver
+    from types import MethodType
+    _instance = [None, False] # contain the instance and whether it is initialized
+    _new = C.__new__
+    _init = C.__init__
+    def _singleton_new(clz, *args, **kw):
+        if not _instance[0]:
+            _instance[0] = _new(C, *args, **kw)
+        return _instance[0]
+    def _singleton_init(self, *args, **kw):
+        if not _instance[1]:
+            _instance[1] = True
+            _init(self, *args, **kw)
+    C.__new__ = staticmethod(_singleton_new)
+    C.__init__ = MethodType(_singleton_init, None, C)
+    return C
+
+# 本来想用Descriptor以便外部更改new和init
+# 但好像只有实例set会使用Descriptor，通过类set就是一般意义的赋值（覆盖）了
+# 而实例创建之后set也没有意义，因为两个函数不会再被调用了，所以就不用了
+# def singleton(c):
+#     '''
+#     singleton pattern
+#     '''
+#     from types import MethodType
+#     _instance = [None, False]
+#     _new = c.__new__
+#     _init = c.__init__
+#     def _real_new(clz, *args, **kw):
+#         if _instance[0] == None:
+#             _instance[0] = _new(c, *args, **kw)
+#         return _instance[0]
+#     def _real_init(self, *args, **kw):
+#         if not _instance[1]:
+#             _instance[1] = True
+#             _init(self, *args, **kw)
+#     class NewDescriptor(object):
+#         def __get__(self, obj, objtype):
+#             return _real_new
+#         def __set__(self, obj, val):
+#             _new = val
+#     class InitDescriptor(object):
+#         def __get__(self, obj, objtype):
+#             def warrper(*args, **kw):
+#                 _real_init(obj, *args, **kw)
+#             return warrper
+#         def __set__(self, obj, val):
+#             _init = val
+#     c.__new__ = NewDescriptor()
+#     c.__init__ = InitDescriptor()
+#     return c
