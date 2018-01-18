500560cccbcc901ebd58e0c63a683d47e2e03ece
Sine <714186139@qq.com>
2018-01-18 16:15:53 +0800

    propertiesReader改为2种读取方式

--- sine/propertiesReader.py
+++ sine/propertiesReader.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 
-def read(path):
+def readAsDict(path):
     f = open(path, 'rb')
     kv = {}
     for line in f:
@@ -8,6 +8,18 @@ def read(path):
         if line.startswith('#') or line == '':
             continue
         tokens = line.split('=', 1)
-        kv[tokens[0].strip()] = tokens[1].strip() if len(tokens) == 2 else True
+        kv[tokens[0].strip()] = tokens[1].strip() if len(tokens) == 2 else ''
     f.close()
     return kv
+
+def readAsList(path):
+    f = open(path, 'rb')
+    l = []
+    for line in f:
+        line = line.replace('\r', '').replace('\n', '').strip()
+        if line.startswith('#') or line == '':
+            continue
+        tokens = line.split('=', 1)
+        l.append((tokens[0].strip(), tokens[1].strip() if len(tokens) == 2 else ''))
+    f.close()
+    return l
