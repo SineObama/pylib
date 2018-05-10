7f8ffa0ef19b84e25410bc06a6e64b179db4471f
Sine <714186139@qq.com>
2018-05-10 15:14:38 +0800

    lib：删除 __init__.py 多余的全部 import

--- sine/__init__.py
+++ sine/__init__.py
@@ -12,11 +12,3 @@ def deleteAllModules():
             del sys.modules[i]
 
 deleteAllModules()
-
-import decorator
-import helpers
-import api
-import threads
-import myPickle
-import sync
-import app
