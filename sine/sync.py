dc1d333b2c5e5f6c4e2fa67ed2e0c6dac3797f7c
Sine <714186139@qq.com>
2018-05-11 15:38:50 +0800

    lib:sync 取消 acquire 和 release，改用 get 获取锁，补充模块自身的线程安全锁；简化代码

--- sine/sync.py
+++ sine/sync.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 '''
-模拟其他语言的线程同步锁（函数装饰器），和同步锁函数acquire和release。
+模拟其他语言的线程同步锁（函数装饰器），和获取可重复同步锁函数get。
 可以看作锁定key对象，也可以把key视作键，使用字符串等。
 key必须是hashable的，必须提供。
 '''
@@ -10,10 +10,13 @@ key必须是hashable的，必须提供。
 import threading as _threading
 import weakref as _weakref
 
+_dLock = _threading.RLock()
 _dict = {}
 def _recycle(key):
+    _dLock.acquire()
     if _dict.has_key(key):
         _dict.pop(key)
+    _dLock.release()
     return
 
 def synchronized(key):
@@ -21,49 +24,30 @@ def synchronized(key):
     线程同步锁（函数装饰器）。
     可以把操作对象本身当做key，也可以是字符串之类的……
     '''
-    try:
-        _key = _weakref.ref(key)
-        if not _dict.has_key(_key):
-            _key = _weakref.ref(key, _recycle)
-            _dict[_key] = _threading.Lock()
-        lock = _dict[_key]
-    except TypeError, e: # 不可创建弱引用（字符串等）
-        if not _dict.has_key(key):
-            _dict[key] = _threading.Lock()
-        lock = _dict[key]
+    lock = get(key)
     def _decorator(func):
         def _decorated(*args, **kw):
             lock.acquire()
             try:
-                rtn = func(*args, **kw)
+                return func(*args, **kw)
             finally:
                 lock.release()
-            return rtn
         return _decorated
     return _decorator
 
-def acquire(key):
+def get(key):
+    _dLock.acquire()
     try:
         _key = _weakref.ref(key)
         if not _dict.has_key(_key):
             _key = _weakref.ref(key, _recycle)
-            _dict[_key] = _threading.Lock()
-        lock = _dict[_key]
-    except TypeError, e:
+            _dict[_key] = _threading.RLock()
+        return _dict[_key]
+    except TypeError, e: # 不可创建弱引用（字符串等）
         if not _dict.has_key(key):
-            _dict[key] = _threading.Lock()
-        lock = _dict[key]
-    lock.acquire()
-    return
+            _dict[key] = _threading.RLock()
+        return _dict[key]
+    finally:
+        _dLock.release()
 
-def release(key):
-    try:
-        key = _weakref.ref(key)
-    except TypeError, e:
-        pass 
-    if _dict.has_key(key):
-        lock = _dict[key]
-        lock.release()
-    return
-
-__all__ = ['synchronized', 'acquire', 'release']
+__all__ = ['synchronized', 'get']
